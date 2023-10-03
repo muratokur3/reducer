af51db4fd029b9e329fd61abb99136c63936eeaf
muratokur3 <muratokur3@icloud.com>
2023-10-03 21:14:41 +0300

    master

--- src/todousereducer/TodoListing.jsx
+++ src/todousereducer/TodoListing.jsx
@@ -16,7 +16,8 @@ const TodoListing = ({state,todos,dispatch}) => {
       <ul style={{display:"flex",flexDirection:"column-reverse"}}>
          {todos.map((todo, index) => (
           todo.completed===true&& <li key={index}>
-            <input onChange={()=>dispatch({type:"CHECK",payload:todo.id})} type='checkbox' checked={todo.completed}/> {todo.text}{" "}
+            <input onChange={()=>dispatch({type:"CHECK",payload:todo.id})} type='checkbox' checked={todo.completed}/>
+            <input onClick={(e)=>e.target.value=e.target.placeholder} placeholder={todo.text} onChange={(e)=>dispatch({type:"UBDATE_CHANGE",payload:e.target.value})} type='text' onBlur={() =>dispatch({ type: "UBDATE_TODO", payload: {id:todo.id,text:state.ubdateTodo} })} /> 
             <button onClick={()=>dispatch({type:"ACTİVE_TODO",payload:{id:todo.id,text:todo.text}})}>Detail</button>
             <button onClick={() => { dispatch({ type: "DELETE_TODO", payload: todo.id }); }} >sil</button>
           
