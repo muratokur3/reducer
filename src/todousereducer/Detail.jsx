3e9e63b5d07d71c3045d3b30782a474eedd7792d
muratokur3 <muratokur3@icloud.com>
2023-10-04 21:29:00 +0300

    master

--- src/todousereducer/Detail.jsx
+++ src/todousereducer/Detail.jsx
@@ -5,7 +5,7 @@ const Detail = ({state,dispatch}) => {
     const details=state.details.filter((detail)=>detail.todoId===state.activeTodo.todoId);
     const activeTodo=state.activeTodo;
     return (
-      <div id=''>
+      <div id='detail-container'>
       <br/>
             <button onClick={()=>dispatch({type:"ACTİVE_TODO",payload:{id:0,text:""}})}>close</button>
       <br/>
@@ -25,7 +25,8 @@ const Detail = ({state,dispatch}) => {
        <ul style={{display:"flex",flexDirection:"column-reverse"}}>
           {details.map((detail, index) => (
            detail.completed===true&& <li key={index}>
-              <input onChange={()=>dispatch({type:"DETAIL_CHECK",payload:detail.id})} type='checkbox' checked={detail.completed}/> {detail.text}{" "}
+              <input onChange={()=>dispatch({type:"DETAIL_CHECK",payload:detail.id})} type='checkbox' checked={detail.completed}/>
+              <input onClick={(e)=>e.target.value=e.target.placeholder} placeholder={detail.text} onChange={(e)=>dispatch({type:"UBDATE_CHANGE",payload:e.target.value})} type='text' onBlur={() =>dispatch({ type: "UBDATE_TODO", payload: {id:detail.id,text:state.ubdateTodo} })} /> 
               <button onClick={() => { dispatch({ type: "DELETE_DETAIL", payload: detail.id }); }} >sil</button>
             </li>
           ))}
--- src/todousereducer/InitialState.js
+++ src/todousereducer/InitialState.js
@@ -1,15 +1,21 @@
 const initialState = {
   todos: [
-    { id: 1, text: "todo1", completed: false },
-    { id: 2, text: "todo2", completed: true },
-    { id: 3, text: "todo3", completed: true },
+    { id: 1, text: "Kahvaltı yap", completed: false },
+    { id: 2, text: "Spor Yap", completed: true },
+    { id: 3, text: "Projeyi tamamla", completed: true },
   ],
   newTodo: "",
   ubdateTodo:"",
   details: [
-    { id: 1, todoId: 1, text: "detail1", completed: false,},
-    { id: 2, todoId: 1, text: "detail2", completed: true },
-    { id: 3, todoId: 2, text: "detail3", completed: true },
+    { id: 1, todoId: 1, text: "Önce ilaç iç", completed: false,},
+    { id: 2, todoId: 1, text: "Bitki çayı unutma", completed: false,},
+    { id: 3, todoId: 1, text: "Minimum tuz", completed: true,},
+    { id: 4, todoId: 2, text: "Spor esnasında 3L su iç", completed: false },
+    { id: 5, todoId: 2, text: "Spordan sonra mutlaka Soğuk duş", completed: true },
+    { id: 6, todoId: 3, text: "Projeyi baştan aşağı oku", completed: true },
+    { id: 7, todoId: 3, text: "Kod tekrarından kaçın", completed: true },
+    { id: 8, todoId: 3, text: "Yeni bir özellik öğren ve ekle", completed: false },
+    { id: 9, todoId: 3, text: "GitHub'a Commitle", completed: false },
   ],
   newDetail: "",
   activeTodo: {
